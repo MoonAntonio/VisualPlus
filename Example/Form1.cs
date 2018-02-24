fca452a7bbf49c0cc717e83718491db999119ed5
DarkByte7 <darkbyte7@live.com>
2018-02-24 03:07:26 -0600

    Minor content addition

--- Example/Form1.cs
+++ Example/Form1.cs
@@ -1,12 +1,17 @@
-﻿using VisualPlus.Toolkit.Controls.Layout;
-
-namespace Example
+﻿namespace Example
 {
+    using VisualPlus.Toolkit.Controls.Layout;
+
+    /// <summary>The <see cref="VisualForm"/> example.</summary>
     public partial class Form1 : VisualForm
     {
+        #region Constructors
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        #endregion
     }
 }
