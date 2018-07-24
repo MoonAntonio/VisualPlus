d659612068a541aee0d9950e7b7cc4599982b3e8
DarkByte7 <darkbyte7@live.com>
2018-07-24 11:25:43 -0500

    Update parameter name

--- VisualPlus/Managers/BrushManager.cs
+++ VisualPlus/Managers/BrushManager.cs
@@ -12,26 +12,26 @@ namespace VisualPlus.Managers
         #region Methods
 
         /// <summary>Creates a glow brush from the specified graphics path.</summary>
-        /// <param name="centerColor">The center color of path gradient.</param>
-        /// <param name="surroundColor">The array of colors correspond to the points in the path.</param>
+        /// <param name="center">The center color of path gradient.</param>
+        /// <param name="surrounding">The array of colors correspond to the points in the path.</param>
         /// <param name="point">The focus point for the gradient offset.</param>
         /// <param name="graphicsPath">The graphics path.</param>
         /// <param name="wrapMode">The wrap mode.</param>
         /// <returns>The <see cref="Brush" />.</returns>
-        public static Brush GlowBrush(Color centerColor, Color[] surroundColor, PointF point, GraphicsPath graphicsPath, WrapMode wrapMode = WrapMode.Clamp)
+        public static Brush GlowBrush(Color center, Color[] surrounding, PointF point, GraphicsPath graphicsPath, WrapMode wrapMode = WrapMode.Clamp)
         {
-            return new PathGradientBrush(graphicsPath) { CenterColor = centerColor, SurroundColors = surroundColor, FocusScales = point, WrapMode = wrapMode };
+            return new PathGradientBrush(graphicsPath) { CenterColor = center, SurroundColors = surrounding, FocusScales = point, WrapMode = wrapMode };
         }
 
         /// <summary>Creates a glow brush from the specified the specified points.</summary>
-        /// <param name="centerColor">The center color of path gradient.</param>
-        /// <param name="surroundColor">The array of colors correspond to the points in the path.</param>
+        /// <param name="center">The center color of path gradient.</param>
+        /// <param name="surrounding">The array of colors correspond to the points in the path.</param>
         /// <param name="point">The focus point for the gradient offset.</param>
         /// <param name="wrapMode">The wrap mode.</param>
         /// <returns>The <see cref="Brush" />.</returns>
-        public static Brush GlowBrush(Color centerColor, Color[] surroundColor, PointF[] point, WrapMode wrapMode = WrapMode.Clamp)
+        public static Brush GlowBrush(Color center, Color[] surrounding, PointF[] point, WrapMode wrapMode = WrapMode.Clamp)
         {
-            return new PathGradientBrush(point) { CenterColor = centerColor, SurroundColors = surroundColor, WrapMode = wrapMode };
+            return new PathGradientBrush(point) { CenterColor = center, SurroundColors = surrounding, WrapMode = wrapMode };
         }
 
         #endregion
