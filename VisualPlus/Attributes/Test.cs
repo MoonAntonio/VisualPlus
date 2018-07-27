5decbe6ad3f5f1b7b39779b7317fccd9689cfbd4
DarkByte7 <darkbyte7@live.com>
2018-07-26 23:41:06 -0500

    Update attribute

--- VisualPlus/Attributes/Test.cs
+++ VisualPlus/Attributes/Test.cs
@@ -7,11 +7,46 @@ using System;
 namespace VisualPlus.Attributes
 {
     /// <summary>Marks the program elements that need to be tested further.</summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true)]
     public class Test : Attribute
     {
+        #region Variables
+
+        private string text;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a new instance of the <see cref="Test" /> class.</summary>
+        /// <param name="text">The text.</param>
+        public Test(string text)
+        {
+            Text = text;
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Test" /> class.</summary>
+        public Test()
+        {
+            text = string.Empty;
+        }
+
+        #endregion
+
         #region Properties
 
-        public object Tag { get; set; }
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+            }
+        }
 
         #endregion
     }
